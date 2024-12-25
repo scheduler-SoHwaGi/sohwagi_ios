a7172be4d2b1b81a4913d7aaf191135073adca51
GuNayeon77 <listoss456@gmail.com>
2024-12-25 20:17:50 +0900

    Initial Commit

---
+++ sohwagi_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sohwagi_ios
+//
+//  Created by 구나연 on 12/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sohwagi_ios/sohwagi_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sohwagi_iosApp.swift
+//  sohwagi_ios
+//
+//  Created by 구나연 on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct sohwagi_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
